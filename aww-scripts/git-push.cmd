39a0adf0c2c2c89b88a258d09e41c6fdd1d1dccb
Gogs <gogs@fake.local>
2022-11-28 09:19:17 -0800

    $message

---
+++ aww-scripts/git-push.cmd
@@ -0,0 +1,15 @@
+@rem combine all the arguments into a single string
+@rem and pass it to git commit -am
+
+set message=%*
+
+@rem if message is empty, set it to "update currentDate"
+if "%message%"=="" (
+  set message=update %currentDate%
+)
+
+git add --all
+git commit -am "$message"
+git pull
+git push
+git status
