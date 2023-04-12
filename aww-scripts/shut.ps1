89fdfbce08d0330506ab157b9b043f90bc307969
Dmytro Zharii <dmytrozharii@microsoft.com>
2023-04-11 22:53:24 -0700

    update Tue 04/11/2023

--- aww-scripts/shut.ps1
+++ aww-scripts/shut.ps1
@@ -3,6 +3,16 @@ param (
     [string]$At
 )
 
+# Check for administrative privileges
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    $scriptPath = $MyInvocation.MyCommand.Path
+    Start-Process PowerShell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`" -At `"$At`"" -Verb RunAs
+    exit
+}
+
+# Set error action preference to stop on first error
+$ErrorActionPreference = "Stop"
+
 # Set task name
 $taskName = "SHUT UP AND SHUT DOWN!"
 
@@ -15,7 +25,7 @@ $task = Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue
 if ($task -eq $null) {
     # Create the task if it doesn't exist
     $action = New-ScheduledTaskAction -Execute "C:\Windows\System32\shutdown.exe" -Argument "/s /t 120"
-    $trigger = New-ScheduledTaskTrigger -Daily -At "$hour:$minute"
+    $trigger = New-ScheduledTaskTrigger -Daily -At "$($hour):$($minute)"
     $settings = New-ScheduledTaskSettingsSet
     $principal = New-ScheduledTaskPrincipal -UserID "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
     $task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings -Principal $principal
@@ -23,8 +33,9 @@ if ($task -eq $null) {
     Register-ScheduledTask -TaskName $taskName -InputObject $task
 } else {
     # Update the trigger time if the task exists
-    $task.Triggers.StartBoundary = (Get-Date -Hour $hour -Minute $minute -Second 0).ToString("s")
-    Set-ScheduledTask -TaskName $taskName -InputObject $task
+    $task.Triggers[0].StartBoundary = (Get-Date -Hour $hour -Minute $minute -Second 0).ToString("s")
+    Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
+    Register-ScheduledTask -TaskName $taskName -InputObject $task
 }
 
-Write-Host "Task '$taskName' updated or created with trigger time $hour:$minute"
+Write-Host "Task '$taskName' updated or created with trigger time $($hour):$($minute)"
