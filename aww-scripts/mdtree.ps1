983aadcf3f5165935ce27a2a918ab0c2e957985a
Dmytro Zharii <you@example.com>
2023-06-21 15:18:57 -0700

    update Wed 06/21/2023

---
+++ aww-scripts/mdtree.ps1
@@ -0,0 +1,35 @@
+param (
+    [string]$Path = ".",
+    [string]$Output = "output.md"
+)
+
+$ErrorActionPreference = "Stop"
+
+# $host.ui.RawUI.WindowTitle = "My Title"
+$ThisScriptFolderPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+
+$extensions = @(
+    "*.scala"
+    "*.json"
+    "*.sh"
+    "*.ps1"
+)
+
+$files = Get-ChildItem -Path $Path -Include $extensions -Recurse
+
+
+"Files: $files"
+
+$markdown = ""
+
+foreach ($file in $files) {
+    $markdown += "## $($file.Name)`n`n"
+    $markdown += "``````$($file.Extension.Substring(1))`n"
+    $markdown += Get-Content $file.FullName -Raw
+    $markdown += "`n``````"
+    $markdown += "`n`n"
+}
+
+$markdown | Out-File $Output -Encoding utf8
+
