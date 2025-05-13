8515d32bd8e9fea74e14a1e0d07ff38877fa174b
Dmytro Zharii <dmytrozharii@microsoft.com>
2025-05-13 14:18:25 -0700

    update Tue 05/13/2025

--- aww-scripts/gitools-git-apply-patch-pr-to-main-windows.cmd
+++ aww-scripts/gitools-git-apply-patch-pr-to-main-windows.cmd
@@ -6,6 +6,25 @@ setlocal
 
 set "mainBranch=%~1"
 set "prBranch=%~2"
+
+git checkout "%prBranch%"
+if ERRORLEVEL 1 (
+    exit /b %ERRORLEVEL%
+)
+git pull
+if ERRORLEVEL 1 (
+    exit /b %ERRORLEVEL%
+)
+
+git checkout "%mainBranch%"
+if ERRORLEVEL 1 (
+    exit /b %ERRORLEVEL%
+)
+git pull
+if ERRORLEVEL 1 (
+    exit /b %ERRORLEVEL%
+)
+
 git diff --merge-base "%mainBranch%" "%prBranch%" | git apply --index
 if ERRORLEVEL 1 (
     exit /b %ERRORLEVEL%
