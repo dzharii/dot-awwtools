ccd9b69991f0cd372f82c4a4fc09058d7977a1a2
John Doe <johndoe@example.com>
2025-12-03 21:35:03 -0800

    update Wed 12/03/2025

--- aww-scripts/shutdown.cmd
+++
@@ -1,6 +0,0 @@
-@rem check if argument "minutest" is not empty and it is a number
-@rem if not -- exit with error
-
-
-
-echo TODO!
--- aww-scripts/update.cmd
+++
@@ -1,8 +0,0 @@
-:: Use pushd to change the current directory to the location of this batch file (%~dp0)
-pushd %~dp0
-
-:: Use call to run the git pull command, which updates your local repository with the latest changes from the remote repository
-call git pull
-
-:: Use popd to return to the original directory
-popd
